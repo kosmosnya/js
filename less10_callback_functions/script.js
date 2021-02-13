6ea8fcbd010c01b7f69f8dc8e85caac1a8bb34cf
kosmosnya <kosmosnya@gmail.com>
2021-02-13 20:53:52 +0100

    functions timeout, callback

---
+++ less10_callback_functions/script.js
@@ -0,0 +1,28 @@
+'use strict' // ES6
+
+// setTimeout on functionc
+
+function first() {
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+} 
+
+function done() {
+    console.log('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done);
