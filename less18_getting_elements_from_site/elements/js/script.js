aa1efebcc3ea0717e003d19220dad4b1bbb23973
kosmosnya <kosmosnya@gmail.com>
2021-02-17 16:34:30 +0100

    getting elements from DOM

---
+++ less18_getting_elements_from_site/elements/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button'); // always creates HTMLCollection pseudoarray
+
+console.log(btns[1]);
+
+
+const circles = document.getElementsByClassName('circle'); // also creates HTMLCollection pseudoarray
+
+console.log(circles);
+
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('div'); // looks for first selector 
+
+console.log(oneHeart);
