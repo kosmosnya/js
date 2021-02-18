c89e652ee3c9e4c56c777fa7548ca3209ad52b0f
kosmosnya <kosmosnya@gmail.com>
2021-02-18 13:42:39 +0100

    practice with site_interactive elements changes

---
+++ less20_practice_elements/project/js/script.js
@@ -0,0 +1,57 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+// 1
+
+const ads = document.querySelectorAll('.promo__adv img');
+
+ads.forEach(item => {
+    item.remove();
+}); 
+
+const poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      moviesList = document.querySelector('.promo__interactive-list');
+
+genre.textContent = 'ДРАМА';
+
+poster.style.cssText = `background:url("/less20_practice_elements/project/img/bg.jpg");`;
+
+moviesList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((movie, i) => {
+    moviesList.innerHTML += `<li class="promo__interactive-item">${i+1}. ${movie}
+    <div class="delete"></div>
+</li>`;
+});
+
+
+// moviesList.forEach(item => {
+//     item.textContent = movieDB.movies;
+// });
+
